7c09f53d597fa03d028724c4a9397703c49f4481
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-09-25 22:53:47 -0400

    formatted the description

--- src/components/item/stat-block/StatBlock.vue
+++ src/components/item/stat-block/StatBlock.vue
@@ -2,7 +2,22 @@
   <div class="stat-block-wrapper">
     <StatBlockBorder />
     <div class="content-container">
-      {{ item.name }}
+      <div class="name accent-color">{{ item.name }}</div>
+      <span class="subtext">
+        {{ item.itemType }}
+        <span v-if="item.itemSubType.length > 0">
+          ({{ item.itemSubType }})
+        </span>
+        , {{ item.rarity }}
+        <span v-if="item.reqAttune">
+          (requires attunement<span v-if="item.attuneRequirements.length > 0">
+            {{ item.attuneRequirements }}</span
+          >)
+        </span>
+      </span>
+      <hr class="line-break" />
+
+      <div class="description">{{ item.description }}</div>
     </div>
     <StatBlockBorder />
   </div>
@@ -10,6 +25,7 @@
 
 <script>
 import StatBlockBorder from "./StatBlockBorder.vue";
+
 export default {
   name: "StatBlock",
   props: ["item"],
@@ -19,7 +35,6 @@ export default {
 
 <style scoped>
 .stat-block-wrapper {
-  white-space: "pre-wrap";
   width: 700px;
   margin-left: auto;
   margin-right: auto;
@@ -29,5 +44,33 @@ export default {
   background-color: #fdf1dc;
   margin: 0 4px;
   padding: 16px;
+}
+
+.accent-color {
+  color: #58170d;
+}
+
+.name {
+  font-size: 24px;
+  font-variant: small-caps;
+  font-family: Georgia, "Times New Roman", Times, serif;
+  font-weight: bold;
+  letter-spacing: 1px;
+  line-height: 1;
+}
+
+.subtext {
+  font-size: 12px;
+  font-style: italic;
+}
+
+.line-break {
+  border: 1px solid #99351f;
+}
+
+.description {
+  white-space: pre-wrap;
+  padding-top: 8px;
+  font-size: 14px;
 }
 </style>
