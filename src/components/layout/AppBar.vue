593587f2b4e4515fd8610db5afe3ffd8fcaa8163
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-09-25 20:11:01 -0400

    App bar and vuetify added

---
+++ src/components/layout/AppBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar color="#004d00" dark>
+    <img :src="logo" /> 5e Homebrew Items
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+  data() {
+    return {
+      logo: require("../../assets/logo.svg"),
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
