b0fd06c6049a37a0f3e30e38f3e246b194f552bc
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-09-25 22:14:08 -0400

    added stat block

---
+++ src/components/item/stat-block/StatBlock.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="stat-block-wrapper">
+    <StatBlockBorder />
+    <div class="content-container">
+      {{ item.name }}
+    </div>
+    <StatBlockBorder />
+  </div>
+</template>
+
+<script>
+import StatBlockBorder from "./StatBlockBorder.vue";
+export default {
+  name: "StatBlock",
+  props: ["item"],
+  components: { StatBlockBorder },
+};
+</script>
+
+<style scoped>
+.stat-block-wrapper {
+  white-space: "pre-wrap";
+  width: 700px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.content-container {
+  background-color: #fdf1dc;
+  margin: 0 4px;
+  padding: 16px;
+}
+</style>
